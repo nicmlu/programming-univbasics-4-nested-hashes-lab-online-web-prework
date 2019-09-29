16236bcc83cfab5097ca9114f3f6d79266675144
Nicole Lucien <nicolemlucien@gmail.com>
2019-09-29 18:07:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,9 +37,11 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
+  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -78,9 +81,9 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
-    
+ 		}
+ 	programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+ 
 end
 
 def changing_alan
@@ -101,8 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
